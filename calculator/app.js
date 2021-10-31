f2038bba7f7dd4ab4ff9e4efabb598272172e5d8
최현호 <x7788@naver.com>
2021-10-31 22:13:53 +0900

    isNaN, if문

--- calculator/app.js
+++ calculator/app.js
@@ -1,3 +1,11 @@
 const age = parseInt(prompt("How old are you?"));
 
-console.log(age);
+
+if(isNaN(age)){
+    //condition === true
+    //조건이 true 일 경우, 여기서 끝난다. 
+    console.log("Please write a number")
+}else{
+    //condition ===false
+    console.log("Thank you for writing your age.")
+}
