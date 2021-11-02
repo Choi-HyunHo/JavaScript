63a5bf800dee929c79111d949bfb0b97cbcbab21
최현호 <x7788@naver.com>
2021-11-03 00:05:29 +0900

    classList 연습

--- browser/app.js
+++ browser/app.js
@@ -1,14 +1,14 @@
 const title = document.querySelector(".name h1");
 
 function handleTitleClick(){
-    const currentColor = title.style.color;
-    let newColor;
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    } else{
-        newColor = "blue";
-    }
-    title.style.color = newColor;
+    const clickedClass = "clicked";
+  if(title.classList.contains(clickedClass)){ // classList 가 clicked를 포함하고 있는지만을 확인
+    //만약 clickedClass title의 classList에 포함되어 있다면
+      title.classList.remove(clickedClass);
+  } else {
+    //만약 clickedClass title의 classList에 포함되어 있지 않다면,
+      title.classList.add(clickedClass);
+  }
 }
 
 
