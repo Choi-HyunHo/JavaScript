9a860fff3101e8e885a25895512d091efda3db06
최현호 <x7788@naver.com>
2021-11-02 22:40:27 +0900

    클릭 이벤트 연습

--- browser/app.js
+++ browser/app.js
@@ -4,4 +4,15 @@ function handleTitleClick(){
     title.style.color = "blue";
 }
 
+function handleMouseEnter(){
+    title.innerText = "mouse is here";
+}
+
+function handleMouseLeave(){
+    title.innerText = "mouse is over";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave);
+
