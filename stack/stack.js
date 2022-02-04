70619064439329e138244981e6d09c86085fb01a
com <x7788@naver.com>
2022-02-04 21:21:21 +0900

    stack 연습 코드

---
+++ stack/stack.js
@@ -0,0 +1,23 @@
+function second(){
+    console.log('hello');
+    return;
+}
+
+function first(){
+    second()
+    return;
+}
+
+function main(){
+    first()
+    return;
+}
+
+main()
+
+
+function End(){
+    End()
+}
+
+End()
